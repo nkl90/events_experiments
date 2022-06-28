456afe60fe41bdfd1fe8d3d2dd1a126d63486db7
Nikolay Deriglazov <nikinik@inbox.ru>
2022-06-28 11:58:15 +0600

    Configure service bus via yaml to service1

--- service1/config/bundles.php
+++ service1/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     GDXbsv\PServiceBusBundle\PServiceBusBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
